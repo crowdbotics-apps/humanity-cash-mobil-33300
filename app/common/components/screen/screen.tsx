48fb2c61a858b440952a47360946cff7f7c85e49
juan duarte <juan.duarte@crowdbotics.com>
2022-05-17 14:32:50 -0400

    fix

--- app/common/components/screen/screen.tsx
+++ app/common/components/screen/screen.tsx
@@ -6,39 +6,23 @@ import {
   StatusBar,
   View
 } from "react-native"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
 import { ScreenProps } from "./screen.props"
 import { isNonScrolling, offsets, presets } from "./screen.presets"
 import { Header } from "../header/header"
-import { BlurView } from "@react-native-community/blur"
-import styles from "../../screens/home-styles"
-// import { BlurView } from 'expo-blur'
+import { COLOR } from "../../theme"
 
 const isIos = Platform.OS === "ios"
 
 function ScreenWithoutScrolling(props: ScreenProps) {
-  const insets = useSafeAreaInsets()
   const preset = presets.fixed
   const style = props.style || {}
   const backgroundStyle = props.backgroundColor
     ? { backgroundColor: props.backgroundColor }
     : {}
-  const insetStyle = { paddingTop: props.unsafe ? 0 : insets.top }
   const bigHeader = props.bigHeader || false
-  const showBlur = props.showBlur || false
 
   return (
     <>
-      {showBlur && (
-        <BlurView
-          style={styles.BLUR_STYLES}
-          //blur
-          blurType="light"
-          // intensity={10}
-          //blurAmount={5}
-          //reducedTransparencyFallbackColor="white"
-        />
-      )}
       <KeyboardAvoidingView
         style={[preset.outer, backgroundStyle]}
         behavior={isIos ? "padding" : undefined}
@@ -47,7 +31,7 @@ function ScreenWithoutScrolling(props: ScreenProps) {
         <StatusBar
           barStyle={props.statusBar || "light-content"}
           translucent
-          backgroundColor={"transparent"}
+          backgroundColor={COLOR.PALETTE.whiteBackground}
           animated
         />
         {props.showHeader && (
@@ -72,8 +56,6 @@ function ScreenWithScrolling(props: ScreenProps) {
     ? { backgroundColor: props.backgroundColor }
     : {}
   const bigHeader = props.bigHeader || false
-  const showBlur = props.showBlur || false
-
   return (
     <>
       <KeyboardAvoidingView
@@ -85,7 +67,7 @@ function ScreenWithScrolling(props: ScreenProps) {
           <StatusBar
             barStyle={props.statusBar || "dark-content"}
             translucent
-            backgroundColor={"transparent"}
+            backgroundColor={COLOR.PALETTE.whiteBackground}
             animated
           />
           {props.showHeader && (
