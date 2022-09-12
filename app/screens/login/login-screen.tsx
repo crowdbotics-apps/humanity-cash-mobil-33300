e8045c79e7d4094cfcbdabf00f36198cbacb9f88
Rafael Bastos <clemente.rafael@crodbotics.com>
2022-09-12 13:49:02 -0400

    apple login

--- app/screens/login/login-screen.tsx
+++ app/screens/login/login-screen.tsx
@@ -96,17 +96,21 @@ export const LoginScreen = observer(function LoginScreen() {
     const appleAuthRequestResponse = await appleAuth.performRequest({
       requestedOperation: appleAuth.Operation.LOGIN,
       requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
-    }).then((response: any) => {
-      console.log(' response => ', response)
     });
-
     // get current authentication state for user
     // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
-    const credentialState = await appleAuth.getCredentialStateForUser(appleAuthRequestResponse.user);
+    const credentialState = await appleAuth.getCredentialStateForUser(
+      appleAuthRequestResponse.user
+    );
 
     // use credentialState response to ensure the user is authenticated
     if (credentialState === appleAuth.State.AUTHORIZED) {
       // user is authenticated
+      const data = {
+        identityToken: appleAuthRequestResponse.identityToken,
+        fullName: appleAuthRequestResponse.fullName
+      }
+      postSocialLogin(data);
     }
   }
 
