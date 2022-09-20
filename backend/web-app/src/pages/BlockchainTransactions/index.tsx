1510a5e38d412800e839bb30b96c4c3694ccbdc3
JB <juanber2.0@gmail.com>
2022-09-20 09:40:26 -0400

    blockchain transactions

--- backend/web-app/src/pages/BlockchainTransactions/index.tsx
+++ backend/web-app/src/pages/BlockchainTransactions/index.tsx
@@ -4,15 +4,19 @@ import SimpleTable from '../../components/Table'
 import {Badge, Button, Form, Row, Tab, Tabs} from "react-bootstrap";
 import AdminPanelContainer from "../../containers";
 import InputGroup from "react-bootstrap/InputGroup";
-import {ArrowRightIcon, SearchIcon} from "../../components/icons";
+import {ArrowRightIcon, Eyes, SearchIcon} from "../../components/icons";
 import {Filter} from "../Employees/Filter";
 import AdvancedTable, {ADV_DELETE_FIELD_NAME} from "../../components/Table/AdvancedTable";
 import {useApi, useUserStore} from "../../utils";
 import {UserGroup} from "../../components/Table/constants";
 import {genericApiError} from "../../helpers";
-import {truncate} from "../../utils/functions";
+import {getErrorMessages, truncate} from "../../utils/functions";
 import moment from 'moment'
 import styles from './BlockchainTransactions.module.css'
+import Modal from "react-bootstrap/Modal";
+import ModalAlerts from "../../components/Modal/ModalAlerts";
+import Stack from "react-bootstrap/Stack";
+import {toast} from "react-toastify";
 
 
 const wrapHash = (txt:string, )=>{
@@ -33,6 +37,9 @@ const BlockTransactionsPage: React.FC = observer(() => {
   const [Previous, setPrevious] = useState<string|null>(null)
   const [Next, setNext] = useState<string|null>(null)
   const [ShowUsername, setShowUsername] = useState<boolean>(true)
+  const [ShowPasswordModal, setShowPasswordModal] = useState<boolean>(false)
+  const [CurrentIndex, setCurrentIndex] = useState<number|null>(null)
+  const [CurrentItem, setCurrentItem] = useState<any|null>(null)
   const [Password, setPassword] = useState<string>("holamundo")
   const [ColumnsTitles, setColumnsTitles] = useState(COLUMN_TITLES)
   const api = useApi()
@@ -64,14 +71,48 @@ const BlockTransactionsPage: React.FC = observer(() => {
   }
 
 
-  const getItem = (id:number, index:number)=>{
+  const getItem = ()=>{
+    if(CurrentItem === null){
+      return
+    }
     let params:any = {}
     if (ShowUsername){
       params["show_username"] = true
       params["password"] = Password
     }
-    api.getBlockchainTransaction(id, params).then((response: any) => {
-      console.log(response)
+
+    api.getBlockchainTransaction(CurrentItem.id, params).then((result: any) => {
+      console.log(result)
+      if (result.kind === "ok"){
+        const data = result.data
+        const newArray = []
+        for (let obj of Items){
+          if(obj.id === CurrentItem.id){
+
+            let row:any = {
+              id: data.id,
+              hash: wrapHash(data.transaction_id),
+              fromUsername: wrapHash(data.from_username) ,
+              toUsername: <div className={styles.toColumn}><div style={{marginRight:8}}> <ArrowRightIcon /> </div> {wrapHash(data.to_username)}</div>,
+              fromAddress:wrapHash(data.from_address),
+              toAddress: wrapHash(data.to_address),
+              type: data.type,
+              createdAt:<CreatedColumn created={data.created}/>,
+              amount:data.amount,
+              confirmedBlocks: data.confirmations
+            }
+            newArray.push(row)
+          }else{
+            newArray.push(obj)
+          }
+        }
+        setItems(newArray)
+        setShowPasswordModal(false)
+      }else{
+        toast.error(getErrorMessages(result.errors), {
+          position: toast.POSITION.TOP_CENTER
+        });
+      }
     })
 
   }
@@ -82,10 +123,10 @@ const BlockTransactionsPage: React.FC = observer(() => {
 
   const getItems = ()=>{
     let params:any = {page:CurrentPage}
-    if (ShowUsername){
-      params["show_username"] = true
-      params["password"] = Password
-    }
+    // if (ShowUsername){
+    //   params["show_username"] = true
+    //   params["password"] = Password
+    // }
     api.getBlockchainTransactions(params).then((response: any) => {
       if (response.kind === "ok") {
         setPrevious(response.data.previous)
@@ -110,7 +151,10 @@ const BlockTransactionsPage: React.FC = observer(() => {
             row["show"] = (<div >
                 <Button type={"button"} onClick={()=>{
                   setShowUsername(true)
-                  getItem(data.id,index )
+                  setShowPasswordModal(true)
+                  setCurrentItem(row)
+                  setCurrentIndex(index)
+
                 }} className={styles.showButton}>show</Button>
             </div>)
           }
@@ -177,6 +221,15 @@ const BlockTransactionsPage: React.FC = observer(() => {
           </InputGroup>)
     }
 
+    const onSubmit = ()=>{
+      console.log("onsubmit")
+    }
+
+    const handleClose = ()=>{
+      console.log("handle close")
+      setShowPasswordModal(false)
+    }
+
     return (
       <AdminPanelContainer
         search={<SearchInput />}
@@ -204,6 +257,55 @@ const BlockTransactionsPage: React.FC = observer(() => {
               onPrevious={onPreviousPage}/>
           </div>
         </Row>
+
+        <Modal
+          show={ShowPasswordModal}
+          onHide={handleClose}
+          className={''}
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+
+
+              <>
+                <Modal.Header closeButton>
+                  <Modal.Title className='title-modal m-2'>Privacy Requirement</Modal.Title>
+                </Modal.Header>
+                <form onSubmit={onSubmit}>
+                  <Modal.Body           style={{paddingLeft:30, paddingRight:30}}
+                  >
+                    <div>
+                      <h6 className='title-h6'>Please enter your password to confirm</h6>
+                    </div>
+
+
+                    <Form.Group className="mb-3" >
+                      <Form.Label className='form-label'>PASSWORD</Form.Label>
+                      <InputGroup className="mb-3">
+                        <Form.Control
+                          type="password" name="password" value={Password} className='input-large' onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button variant="outline-secondary" id="button-addon2" className='eyes-buttons'>
+                          <Eyes />
+                        </Button>
+                      </InputGroup>
+                    </Form.Group>
+
+
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Stack gap={2} className="col-md-5 mx-auto modal-button mb-4">
+                      <Button variant="primary" onClick={getItem} type="button" >
+                        Confirm
+                      </Button>
+                    </Stack>
+
+                  </Modal.Footer>
+                </form>
+              </>
+        </Modal>
+
+
       </AdminPanelContainer>
     )
 })
