b519bd580079260b0baad7b034d7df07fa1dff5e
jorge <jmesquita@innovatica.com.py>
2022-09-29 04:01:35 -0400

    removed burning coins on transfer creation, should be done pre-creation

--- backend/home/api/v1/viewsets/dwolla_webhooks/transfers/created.py
+++ backend/home/api/v1/viewsets/dwolla_webhooks/transfers/created.py
@@ -12,19 +12,10 @@ def transfer_created_listener(event, event_db):
         if transfer['_links'].source['resource-type'] == 'customer':
             # transfers from customers only matters on proccessed status
             pass
-
         elif transfer['_links'].destination['resource-type'] == 'customer':
             have_profile, profile = get_profile_for_href(transfer, transfer['_links'].destination.href)
             if have_profile:
-                logger.info(f'transaction {transfer.id} created, burning {transfer.amount.value} while transfer '
-                            f'completes')
-                try:
-                    profile.withdraw(transfer.amount.value)
-                    logger.info(
-                        f'burning {transfer.amount.value} from {profile} succesfull')
-                except:
-                    logger.exception(f'error burning {transfer.amount.value} from profile {profile}, fix transaction '
-                                     f'manually')
+                logger.info(f'transaction {transfer.id} created, burning {transfer.amount.value} should be done for {profile}')
             else:
                 logger.warning(f'transfer {transfer.id}, customer {transfer.destination.href} not found, ignoring')
         else:
