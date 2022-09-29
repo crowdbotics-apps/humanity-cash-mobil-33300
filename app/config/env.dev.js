db0fd5753d02710645b6e4784eb29e84933ef40f
Rafael Bastos <clemente.rafael@crodbotics.com>
2022-09-29 11:59:59 -0400

    fix env

--- app/config/env.dev.js
+++ app/config/env.dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   // cambiar
-  // API_URL: "https://humanity-cash-mobil-33300.botics.co",
-  API_URL: "http://192.168.120.10:8000",
+  API_URL: "https://humanity-cash-mobil-33300.botics.co",
+  // API_URL: "http://192.168.120.10:8000",
   // cambiar
   STRIPE_KEY:
     ""
