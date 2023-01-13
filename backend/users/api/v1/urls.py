6720fc769cc8aed91a795a661719c2ee79fcea59
hbittar <hassankarimbittar@gmail.com>
2023-01-13 16:35:42 -0300

    new build

--- backend/users/api/v1/urls.py
+++ backend/users/api/v1/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from users.api.v1.viewsets import UserAdminView
+from users.api.v1.viewsets import UserAdminView, ResetPasswordFormCustom
 
 router = DefaultRouter()
 router.register("user-admin", UserAdminView, basename="user-admin")
 
 urlpatterns = [
+    path('reset-password/', ResetPasswordFormCustom.as_view(), name='api.reset_password'),
     path("", include(router.urls))
 ]
