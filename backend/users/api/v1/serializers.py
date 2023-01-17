7b95b3206ba946b68567153afd092aebb15b9d81
hbittar <hassankarimbittar@gmail.com>
2023-01-17 08:09:26 -0300

    new build

--- backend/users/api/v1/serializers.py
+++ backend/users/api/v1/serializers.py
@@ -10,7 +10,7 @@ class UserDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id','name', 'first_name', 'last_name', 'role', 'group', 'is_admin', 'username']
+        fields = ['id','name', 'first_name', 'last_name', 'role', 'group', 'is_admin', 'username', 'email']
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
@@ -21,10 +21,15 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
 
     def validate_email(self, email):
-        email = get_adapter().clean_email(email)
-        user = User.objects.filter(username=email).exists()
-        if email and (email_address_exists(email) or user):
-            raise serializers.ValidationError("A user is already registered with this e-mail address.")
+        if self.instance:
+            user = User.objects.exclude(id=self.instance.id).filter(email=email).exists()
+            if user:
+                raise serializers.ValidationError("A user is already registered with this e-mail address.")
+        else:
+            email = get_adapter().clean_email(email)
+            user = User.objects.filter(username=email).exists()
+            if email and (email_address_exists(email) or user):
+                raise serializers.ValidationError("A user is already registered with this e-mail address.")
         return email
 
     def validate_role(self, role):
@@ -43,7 +48,8 @@ class UserCreateSerializer(serializers.ModelSerializer):
             email=validated_data.get('email'),
             username=validated_data.get('email'),
             name=validated_data.get('name'),
-            is_admin=True
+            is_admin=True,
+            verified_email=True
         )
         user.save()
         request = self.context.get('request')
