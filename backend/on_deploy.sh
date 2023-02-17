0eb98e332fd71ed595aa0b2ac0ebbb9034a04e3a
jorge <jmesquita@innovatica.com.py>
2023-02-17 14:57:35 -0300

    dont run some commands on deploy anymore

--- backend/on_deploy.sh
+++ backend/on_deploy.sh
@@ -2,5 +2,5 @@
 
 python3 manage.py migrate
 python3 manage.py create_missing_wallets
-python3 manage.py refresh_wallet_addresses
-python3 manage.py refresh_transaction_receipts
+#python3 manage.py refresh_wallet_addresses
+#python3 manage.py refresh_transaction_receipts
