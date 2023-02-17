63dbce7f6340dd641843a61d3f6982dd69939905
jorge <jmesquita@innovatica.com.py>
2023-02-17 14:29:03 -0300

    test new ondeploy

--- on_deploy.sh
+++ on_deploy.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
 python3 manage.py migrate
-python3 manage.py create_missing_wallets
-python3 manage.py refresh_wallet_addresses
-python3 manage.py refresh_transaction_receipts
+#python3 manage.py create_missing_wallets
+#python3 manage.py refresh_wallet_addresses
+#python3 manage.py refresh_transaction_receipts
